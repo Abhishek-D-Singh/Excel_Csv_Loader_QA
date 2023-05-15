68d0c33b56a1197e80cbcbd7bdb32591e3f7a733
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-15 16:22:53 +0530

    Update Excel_Csv_qa.py

--- Excel_Csv_qa.py
+++ Excel_Csv_qa.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.agents import create_csv_agent
 from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
